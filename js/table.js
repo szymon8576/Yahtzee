e7eaeef812528ec871236578d5ae395481fd2d04
szymon8576 <50836217+szymon8576@users.noreply.github.com>
2023-10-02 20:04:09 +0200

    recording button

--- js/table.js
+++ js/table.js
@@ -1,7 +1,17 @@
-const micToggle = document.getElementById('mic-toggle');
+const micOnLabel = document.getElementById('mic-on');
+const micOffLabel = document.getElementById('mic-off');
+const recordingButton = document.getElementById("recordingButton");
+const micButtonText = document.getElementById("micButtonText");
 
-micToggle.addEventListener('click', () => {
-    micToggle.classList.toggle('mic-off');
-    micToggle.classList.toggle('mic-on');
-    console.log('zmiana');
+micOffLabel.style.display="none";
+micButtonText.innerText = "Start recording"
+
+recordingButton.addEventListener('click', () => {
+
+    micButtonText.innerText = (micButtonText.innerText === "Start recording") ? "End recording" : "Start recording"
+
+    micOnLabel.style.display = (micOnLabel.style.display === "none") ? "block" : "none"
+    micOffLabel.style.display = (micOffLabel.style.display === "none") ? "block" : "none"
+
+    console.log('zmiana', micOnLabel.style.display, micOffLabel.style.display);
 });
