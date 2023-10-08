44a683d4b341c2d2751e4df281a0d8ba508717d9
nsoktab <nataliasokolowskaa@gmail.com>
2023-10-08 19:57:58 +0200

    Update scoring.js

--- js/scoring.js
+++ js/scoring.js
@@ -6,81 +6,76 @@ let roundNumber = 1;
 let diceRolled = []; //all dice in play array
 let diceOnTable = []; //on-table dice array
 let diceSelected = []; //selected dices array
-let currentDiceIndex;  //dice selection/deselection
-let diceIndexHolder = [0,1,2,3,4];
+let currentDiceIndex; //dice selection/deselection
+let diceIndexHolder = [0, 1, 2, 3, 4];
 let selectedDiceElements; //HTML Collection
 let selectedCategory = null;
 let lockedDice = [false, false, false, false, false];
-let currentPlayer=1;
+let currentPlayer = 1;
 
 //DOM Elements
-const diceArea=document.getElementsByClassName("dice-display");
+const diceArea = document.getElementsByClassName("dice-display");
 const rollButton = document.getElementById("rollDice");
 
 //events
-rollButton.addEventListener('click', function (){  
-	if(rollNumber<20) 
-	{randomDice(),
-	rollNumber++}
+rollButton.addEventListener("click", function () {
+  if (rollNumber < 20) {
+    randomDice(), rollNumber++;
+  }
 });
 
 randomDice();
 //Random number between 1-6
 function randomDice() {
-	oldDices = diceRolled;
-	diceRolled=[];
-	for (let i = 0; i < 5; i++) {
-		if (lockedDice[i]){
-			diceRolled.push(oldDices[i]);
-		}
-		else{
-			const randomNumber = Math.floor(Math.random() * 6) + 1;
-			diceRolled.push(randomNumber);
-		}
-		
-}
-	console.log(diceRolled);
-	updateDiceImages();
-	displaySpeculativeScore();
+  oldDices = diceRolled;
+  diceRolled = [];
+  for (let i = 0; i < 5; i++) {
+    if (lockedDice[i]) {
+      diceRolled.push(oldDices[i]);
+    } else {
+      const randomNumber = Math.floor(Math.random() * 6) + 1;
+      diceRolled.push(randomNumber);
+    }
+  }
+  console.log(diceRolled);
+  updateDiceImages();
+  displaySpeculativeScore();
 }
 
-
-function displaySpeculativeScore(){
-
-	for (let i=0; i<functionNames.length; i++){
-		document.getElementById(`${functionNames[i].name.toLowerCase()}-${currentPlayer}`).textContent= functionNames[i](diceRolled)
-	}
-	
+function displaySpeculativeScore() {
+  for (let i = 0; i < functionNames.length; i++) {
+    document.getElementById(
+      `${functionNames[i].name.toLowerCase()}-${currentPlayer}`
+    ).textContent = functionNames[i](diceRolled);
+  }
 }
 
-function updateDiceImages(){
-	const diceContainer = document.querySelector(".dice-display")
-	const diceElements = diceContainer.querySelectorAll('.dice');
+function updateDiceImages() {
+  const diceContainer = document.querySelector(".dice-display");
+  const diceElements = diceContainer.querySelectorAll(".dice");
 
-	for (let i = 0; i < diceRolled.length; i++) {
-		if (!lockedDice[i])
-		{
-			const diceImg = diceElements[i];
-			const diceNumber = diceRolled[i];
-			diceImg.src = `/images/dice-${diceNumber}.png`;
-			diceImg.alt = `Dice ${diceNumber}`;
-		}
-		
-	  }
+  for (let i = 0; i < diceRolled.length; i++) {
+    if (!lockedDice[i]) {
+      const diceImg = diceElements[i];
+      const diceNumber = diceRolled[i];
+      diceImg.src = `/images/dice-${diceNumber}.png`;
+      diceImg.alt = `Dice ${diceNumber}`;
+    }
+  }
 }
 
+//lock dices on table and push to selected array
 function toggleLock(index) {
-	lockedDice[index] = !lockedDice[index];
-	const diceElement = document.getElementById(`dice-${index+1}`);
-	if (lockedDice[index]) {
-		diceElement.classList.add("selected");
-		diceSelected.push()
-	} else {
-		diceElement.classList.remove("selected");
-	}
-	
+  lockedDice[index] = !lockedDice[index];
+  const diceElement = document.getElementById(`dice-${index + 1}`);
+  if (lockedDice[index]) {
+    diceElement.classList.add("selected");
+    diceSelected.push();
+  } else {
+    diceElement.classList.remove("selected");
+  }
 }
 
-function chooseScore(){
-	rollNumber = 0;
+function chooseScore() {
+  rollNumber = 0;
 }
