391f3f9a1010fb142af4557a4b64e6c044bfafdb
szymon8576 <50836217+szymon8576@users.noreply.github.com>
2024-01-09 20:58:34 +0100

    bugfix

--- python/app/speech_recognition/routes.py
+++ python/app/speech_recognition/routes.py
@@ -9,7 +9,7 @@ speech_recognition_bp = Blueprint('speech-recognition', __name__)
 @speech_recognition_bp.route('/recognize', methods=["POST"])
 def recognize():
     audio, _ = librosa.load(io.BytesIO(request.files["audio_data"].read()), sr=8000)
-
+    print(len(audio), audio)
     if len(audio)>0:
         recognized_digits = predict_wav(audio)
         return jsonify(recognized_digits)
