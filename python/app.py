30ce154bd967568f7b4ac163c4628bace04ab1b4
szymon8576 <50836217+szymon8576@users.noreply.github.com>
2024-01-09 16:03:19 +0100

    backend config update

--- python/app.py
+++ python/app.py
@@ -5,5 +5,5 @@ app = create_app(debug=True)
 app.config.from_pyfile('config.py')
 CORS(app)
 
-# if __name__ == '__main__':
-# socketio.run(app, host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
